adabfd24a863ae27fb05c2f3a5c5be0b4529ab83
proyectos-svg <abraham301189@gmail.com>
2026-06-07 19:15:57 -0500

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'sumwic-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'sumwic-v2';
+const assets = [
+  './',
+  './index.html',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdn.jsdelivr.net/npm/xlsx-js-style@1.2.0/dist/xlsx.bundle.js'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
